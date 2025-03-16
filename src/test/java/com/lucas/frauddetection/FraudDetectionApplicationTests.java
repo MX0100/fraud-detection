29950c32e0069cbd6592f962d5d421c74d207753
MX0100 <lucaswang0402@gmail.com>
2025-03-16 15:17:10 -0230

    first commit

---
+++ src/test/java/com/lucas/frauddetection/FraudDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.frauddetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
